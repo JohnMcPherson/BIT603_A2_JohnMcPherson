391fe4cde428bc7c241becbebd57ec34a0947b1f
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-05-10 20:25:41 +1200

    Passing test for (initial) error message on click login button

--- app/src/main/java/nz/co/afleet/bit603_a2_johnmcpherson/LoginActivity.java
+++ app/src/main/java/nz/co/afleet/bit603_a2_johnmcpherson/LoginActivity.java
@@ -3,6 +3,9 @@ package nz.co.afleet.bit603_a2_johnmcpherson;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView errorMessage = findViewById(R.id.textErrorMessage);
+
+        Button loginButton = findViewById(R.id.buttonLogin);
+        loginButton.setOnClickListener(v -> {
+            errorMessage.setText("Please Enter: User and Password");
+        });
     }
 }

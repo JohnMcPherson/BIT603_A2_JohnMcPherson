0f8561b78af9ed755a2ae68e4c2b2d1d60cd4e4c
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-05-12 10:56:17 +1200

    New MainActivity

--- app/src/main/java/nz/co/afleet/bit603_a2_johnmcpherson/MainActivity.java
+++ app/src/main/java/nz/co/afleet/bit603_a2_johnmcpherson/MainActivity.java
@@ -1,21 +1,45 @@
-/*
-ASSUMPTIONS
-
-    - minSdk required is 16
-
-*/
-
 package nz.co.afleet.bit603_a2_johnmcpherson;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import nz.co.afleet.bit603_a2_johnmcpherson.ui.main.SectionsPagerAdapter;
+import nz.co.afleet.bit603_a2_johnmcpherson.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }

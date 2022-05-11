3caa97cc9d1a490052c0df13eac56df5c3c263fa
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-05-11 15:20:08 +1200

    Launch Mainactivity from login screen refactor login error messages (inluding using string resources)

--- app/src/main/java/nz/co/afleet/bit603_a2_johnmcpherson/LoginActivity.java
+++ app/src/main/java/nz/co/afleet/bit603_a2_johnmcpherson/LoginActivity.java
@@ -2,6 +2,7 @@ package nz.co.afleet.bit603_a2_johnmcpherson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,22 +22,35 @@ public class LoginActivity extends AppCompatActivity {
 
         Button loginButton = findViewById(R.id.buttonLogin);
         loginButton.setOnClickListener(v -> {
-            String requiredErrorMessage = determineErrorMessage();
-            textViewErrorMessage.setText(requiredErrorMessage);
+            String stringErrorMessage = determineErrorMessage();
+            textViewErrorMessage.setText(stringErrorMessage);
+            if (stringErrorMessage.equals("")) {
+                launchMainActivity();
+            }
         });
+    }
+
+    private void launchMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     private String determineErrorMessage() {
         // TODO split messages, and move to string values
         if (editTextUserName.getText().toString().equals("")) {
             if (editTextPassword.getText().toString().equals("")) {
-                return "Please enter your User Name and Password";
+                return getString(R.string.login_error_header)
+                        + " " + getString(R.string.login_error_user_name)
+                        + " " + getString(R.string.and)
+                        + " " + "Password";
             } else {
-                return "Please enter your User Name";
+                return getString(R.string.login_error_header)
+                        + " " + getString(R.string.login_error_user_name);
             }
         } else {
             if (editTextPassword.getText().toString().equals("")) {
-                return "Please enter your Password";
+                return getString(R.string.login_error_header)
+                        + " " + getString(R.string.login_error_password);
             } else {
                 return "";
             }

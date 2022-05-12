73cc9184fedb5454c0df70b61b28dafc79543d59
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-05-12 15:25:16 +1200

    Start to modify Welcome message

--- app/src/main/java/nz/co/afleet/bit603_a2_johnmcpherson/ui/main/WelcomeFragment.java
+++ app/src/main/java/nz/co/afleet/bit603_a2_johnmcpherson/ui/main/WelcomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,10 +12,17 @@ import androidx.fragment.app.Fragment;
 
 import nz.co.afleet.bit603_a2_johnmcpherson.R;
 
+// getString() has a warning, with root cause that getContext() is nullable. I assumed (and tested) that the context is known by the time
+// onCreateView() is called
+@SuppressWarnings("ConstantConditions")
 public class WelcomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
+        View view = inflater.inflate(R.layout.fragment_welcome, container, false);
+        TextView textViewWelcomeMessage = view.findViewById(R.id.textViewWelcomeMessage);
+        String stringWelcomeMessage = getContext().getString(R.string.welcome_message_header);
+        textViewWelcomeMessage.setText(stringWelcomeMessage);
+        return view;
     }
 }

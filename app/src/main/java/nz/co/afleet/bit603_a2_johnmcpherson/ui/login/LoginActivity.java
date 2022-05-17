8f0e6105574a9d5a0afe4c7d3f8dfa7671db1535
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-05-18 10:05:32 +1200

    IntegrationTest_Login testfor incorrect user/password combination : pass.

--- app/src/main/java/nz/co/afleet/bit603_a2_johnmcpherson/ui/login/LoginActivity.java
+++ app/src/main/java/nz/co/afleet/bit603_a2_johnmcpherson/ui/login/LoginActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import nz.co.afleet.bit603_a2_johnmcpherson.R;
+import nz.co.afleet.bit603_a2_johnmcpherson.User;
 import nz.co.afleet.bit603_a2_johnmcpherson.ui.main.MainActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -28,8 +29,13 @@ public class LoginActivity extends AppCompatActivity {
             boolean hasUserName = !editTextUserName.getText().toString().isEmpty();
             boolean hasPassword = !editTextPassword.getText().toString().isEmpty();
             if (hasUserName && hasPassword) {
-                textViewErrorMessage.setText("");
-                launchMainActivity();
+                boolean loginSuccessful = User.loginUser(editTextUserName.getText().toString(), editTextPassword.getText().toString());
+                if (loginSuccessful) {
+                    textViewErrorMessage.setText("");
+                    launchMainActivity();
+                } else {
+                    textViewErrorMessage.setText(R.string.error_incorrect_login) ;
+                }
             } else {
                 String stringErrorMessage = determineErrorMessage(hasUserName, hasPassword);
                 textViewErrorMessage.setText(stringErrorMessage);
@@ -50,7 +56,7 @@ public class LoginActivity extends AppCompatActivity {
                 errorMessage += (" " + getString(R.string.and) + " " + getString(R.string.login_error_password));
             }
         } else {
-            // we don't need to check hasPassword. The method should not have been called if userName and Password are both empty
+            // For this path, we don't need to check hasPassword. The method should not have been called if userName and Password are both empty
             errorMessage += (getString(R.string.login_error_password));
         }
         return errorMessage;

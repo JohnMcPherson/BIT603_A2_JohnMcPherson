d8c6d04bd2f9144c98637c8d68dc180ce9278b86
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-05-19 10:54:26 +1200

    testCreateInventoryItem() (works)

--- app/src/test/java/nz/co/afleet/bit603_a2_johnmcpherson/IntegrationTestDatabase.java
+++ app/src/test/java/nz/co/afleet/bit603_a2_johnmcpherson/IntegrationTestDatabase.java
@@ -9,22 +9,45 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(RobolectricTestRunner.class)
 public class IntegrationTestDatabase {
     private Application application;
     InventoryDatabase inventoryDatabase;
+    DaoInventory daoInventory;
+
+    private final String SUGAR = "Sugar";
+    private final double SUGAR_QUANTITY = 4;
+    private final String FLOUR = "Flour";
+    private final double FLOUR_QUANTITY = 6.6;
+
+
 
     @Before
     public void initialiseApplicationInstance() {
         Application application = ApplicationProvider.getApplicationContext();
         inventoryDatabase = InventoryDatabase.getInstance(application);
+        daoInventory = inventoryDatabase.daoInventory();
     }
 
     @Test
-    public void testCanGetDatabaseInstance() {
-        assertNotNull(inventoryDatabase);
-     }
+    public void testCreateInventoryItem() {
+        InventoryItem sugarInventory = new InventoryItem();
+        sugarInventory.setName(SUGAR);
+        sugarInventory.setQuantity(SUGAR_QUANTITY);
 
+        assertEquals(sugarInventory.getName(), SUGAR);
+        // test with whole number
+        assertEquals(sugarInventory.getQuantity(), SUGAR_QUANTITY, 0);
+
+        InventoryItem flourInventory = new InventoryItem();
+        flourInventory.setName(FLOUR);
+        flourInventory.setQuantity(FLOUR_QUANTITY);
+
+        assertEquals(flourInventory.getName(), FLOUR);
+        // test with decimal number
+        assertEquals(flourInventory.getQuantity(), FLOUR_QUANTITY, 0);
+    }
 }

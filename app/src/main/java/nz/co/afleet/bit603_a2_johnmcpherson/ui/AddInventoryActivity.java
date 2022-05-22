cad5469b199a2557467807adb9db9d879f9298a6
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-05-23 10:35:28 +1200

    Back button works now

--- app/src/main/java/nz/co/afleet/bit603_a2_johnmcpherson/ui/AddInventoryActivity.java
+++ app/src/main/java/nz/co/afleet/bit603_a2_johnmcpherson/ui/AddInventoryActivity.java
@@ -7,6 +7,7 @@ import nz.co.afleet.bit603_a2_johnmcpherson.R;
 
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class AddInventoryActivity extends AppCompatActivity {
 
@@ -15,7 +16,20 @@ public class AddInventoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_inventory);
 
+        // show the "Home" menu item (to return us to the calling screen)
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // act on the "Home" menu item
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

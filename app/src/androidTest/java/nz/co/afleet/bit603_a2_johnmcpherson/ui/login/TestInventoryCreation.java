4c7c215a50c46af2c6269ad963c1f4a390eb611c
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-05-25 17:40:53 +1200

    Extra delays in test

--- app/src/androidTest/java/nz/co/afleet/bit603_a2_johnmcpherson/ui/login/TestInventoryCreation.java
+++ app/src/androidTest/java/nz/co/afleet/bit603_a2_johnmcpherson/ui/login/TestInventoryCreation.java
@@ -458,7 +458,7 @@ public class TestInventoryCreation {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(700);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -475,7 +475,7 @@ public class TestInventoryCreation {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(700);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
